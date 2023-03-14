df4ef11e8f125dc75c51a789cd8833627a1b4842
Amith H D <103755649+amithhd@users.noreply.github.com>
2023-03-14 15:50:41 +0530

    Update p2.c

--- p2.c
+++ p2.c
@@ -1,1 +1,72 @@
-#
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+#include<stdio.h>
+ 
+int main()
+{
+    int w,i,f,frames[50];
+ 
+    printf("Enter window size: ");
+    scanf("%d",&w);
+ 
+    printf("\nEnter number of frames to transmit: ");
+    scanf("%d",&f);
+ 
+    printf("\nEnter %d frames: ",f);
+ 
+    for(i=1;i<=f;i++)
+        scanf("%d",&frames[i]);
+ 
+    printf("\nWith sliding window protocol the frames will be sent in the following manner (assuming no corruption of frames)\n\n");
+    printf("After sending %d frames at each stage sender waits for acknowledgement sent by the receiver\n\n",w);
+ 
+    for(i=1;i<=f;i++)
+    {
+        if(i%w==0)
+        {
+            printf("%d\n",frames[i]);
+            printf("Acknowledgement of above frames sent is received by sender\n\n");
+        }
+        else
+            printf("%d ",frames[i]);
+    }
+ 
+    if(f%w!=0)
+        printf("\nAcknowledgement of above frames sent is received by sender\n");
+ 
+    return 0;
+}
